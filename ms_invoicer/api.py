60fb4fec448c95cf8589ff4969c21fde527a9074
Mateo Salcedo Rodriguez <msalcedo@domaintools.com>
2023-08-01 21:56:29 -0500

    generate invoice without file

--- ms_invoicer/api.py
+++ ms_invoicer/api.py
@@ -2,7 +2,7 @@ import logging
 from datetime import datetime
 from typing import List
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
@@ -14,6 +14,9 @@ from ms_invoicer.security_helper import get_current_user
 from ms_invoicer.sql_app import crud, schemas
 from ms_invoicer.utils import create_folders
 from ms_invoicer.no_upload_helper import populate_db
+from ms_invoicer.dao import PdfToProcessEvent
+from ms_invoicer.event_bus import publish
+from pydantic import BaseModel
 
 create_folders()
 populate_db()
@@ -179,6 +182,22 @@ def get_services(
     return crud.get_services(db=db, current_user_id=current_user.id)
 
 
+@api.post("/service", response_model=List[schemas.Service])
+def post_service(
+    contracts: List[schemas.ServiceCreate],
+    current_user: schemas.User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    result = []
+    for contract in contracts:
+        obj_dict = contract.dict()
+        obj_dict["user_id"] = current_user.id
+        result.append(
+            crud.create_service(db=db, model=schemas.ServiceCreate(**obj_dict))
+        )
+    return result
+
+
 @api.get("/topinfo", response_model=list[schemas.TopInfo])
 def get_topinfos(
     current_user: schemas.User = Depends(get_current_user),
@@ -201,3 +220,100 @@ def update_topinfo(
         return crud.get_topinfos(db=db, current_user_id=current_user.id)
     else:
         return None
+
+
+class Generate_PDF(BaseModel):
+    bill_to_id: str
+    invoice: schemas.InvoiceBase
+    contracts: List[schemas.ServiceCreateNoFile]
+    use_existing_invoice: bool = False
+    with_taxes: bool = True
+
+
+@api.post("/generate_pdf")
+async def generate_pdf(
+    body: Generate_PDF,
+    current_user: schemas.User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    result = crud.get_invoices_by_number_id(
+        db=db,
+        number_id=body.invoice.number_id,
+        customer_id=body.invoice.customer_id,
+        current_user_id=current_user.id,
+    )
+    if not result or body.use_existing_invoice:
+        try:
+            if result and body.use_existing_invoice:
+                new_invoice = result
+            else:
+                obj_dict = body.invoice.dict()
+                obj_dict["user_id"] = current_user.id
+                new_invoice = crud.create_invoice(
+                    db=db, model=schemas.InvoiceCreate(**obj_dict)
+                )
+            file_obj = schemas.FileCreate(
+                **{
+                    "s3_xlsx_url": None,
+                    "s3_pdf_url": None,
+                    "created": datetime.now(),
+                    "invoice_id": new_invoice.id,
+                    "bill_to_id": body.bill_to_id,
+                    "user_id": current_user.id,
+                }
+            )
+            new_file = crud.create_file(db=db, model=file_obj)
+            result = []
+            for contract in body.contracts:
+                obj_dict = contract.dict()
+                obj_dict["user_id"] = current_user.id
+                obj_dict["invoice_id"] = new_invoice.id
+                obj_dict["file_id"] = new_file.id
+                result.append(
+                    crud.create_service(db=db, model=schemas.ServiceCreate(**obj_dict))
+                )
+            template_name = "template01.html"
+            if not body.with_taxes:
+                template_name = "template03.html"
+            data_event = PdfToProcessEvent(
+                current_user_id=current_user.id,
+                invoice=new_invoice,
+                file=new_file,
+                html_template_name=template_name,
+                xlsx_url=None,
+                with_file=False,
+                with_taxes=body.with_taxes
+            )
+            log.info("Customer {} - Publishing pdf event".format(current_user.id))
+            await publish(data_event)
+            return crud.get_file(
+                db=db, model_id=new_file.id, current_user_id=current_user.id
+            )
+        except Exception as e:
+            log.error("Customer {} - {}".format(current_user.id, e))
+            files = crud.get_files_by_invoice(
+                db=db, model_id=new_invoice.id, current_user_id=current_user.id
+            )
+            for file in files:
+                crud.delete_services_by_file(
+                    db=db, model_id=file.id, current_user_id=current_user.id
+                )
+            crud.delete_files_by_invoice(
+                db=db, model_id=new_invoice.id, current_user_id=current_user.id
+            )
+            crud.delete_invoice(
+                db=db, model_id=new_invoice.id, current_user_id=current_user.id
+            )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Existe una factura con ese numéro de factura",
+        )
+
+
+@api.get("/template", response_model=list[schemas.Template])
+def get_templates(
+    current_user: schemas.User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    return crud.get_templates(db=db, current_user_id=current_user.id)
--- ms_invoicer/dao.py
+++ ms_invoicer/dao.py
@@ -10,6 +10,7 @@ class FilesToProcessData:
     file_id: int
     invoice_id: int
     current_user_id: int
+    with_taxes: int
 
     def __init__(
         self,
@@ -20,6 +21,7 @@ class FilesToProcessData:
         price_unit: int,
         col_letter: str,
         currency: str,
+        with_taxes: bool,
     ) -> None:
         self.xlsx_url = file_path
         self.file_id = file_id
@@ -28,9 +30,10 @@ class FilesToProcessData:
         self.price_unit = price_unit
         self.col_letter = col_letter
         self.currency = currency
+        self.with_taxes = with_taxes
 
 
-class FilesToProcessEvent(Event):
+class FilesToProcessEvent(Event): #TODO: Remove this class
     """
     TODO: Add description
     """
@@ -51,12 +54,16 @@ class PdfToProcessEvent(Event):
         file: File,
         html_template_name: str,
         xlsx_url: str,
+        with_file: bool = True,
+        with_taxes = True,
     ):
         self.current_user_id = current_user_id
         self.invoice = invoice
         self.file = file
         self.html_template_name = html_template_name
         self.xlsx_url = xlsx_url
+        self.with_file = with_file
+        self.with_taxes = with_taxes
 
 
 class GenerateFinalPDF(Event):
@@ -76,6 +83,23 @@ class GenerateFinalPDF(Event):
         self.current_user_id = current_user_id
         self.path_pdf_tables = pdf_tables
         self.xlsx_url = xlsx_url
+        self.path_pdf_invoice = pdf_invoice
+        self.filename = filename
+        self.file_id = file_id
+
+class GenerateFinalPDFNoFile(Event):
+    """
+    TODO: Add description
+    """
+
+    def __init__(
+        self,
+        current_user_id: int,
+        pdf_invoice: str,
+        filename: str,
+        file_id: int,
+    ):
+        self.current_user_id = current_user_id
         self.path_pdf_invoice = pdf_invoice
         self.filename = filename
         self.file_id = file_id
